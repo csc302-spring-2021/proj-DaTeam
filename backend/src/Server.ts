61a74f17e352e789ca1c2c1f8588e098dc72d64e
Zhenyao She <dr.tobyshe@gmail.com>
2021-02-14 14:10:06 -0500

    feat: update env template

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -13,8 +13,7 @@ const debug = Debug("tecl-backend:server");
 /**
  * Get port from environment and store in Express.
  */
-
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.APP_PORT || "3000");
 app.set("port", port);
 
 /**
