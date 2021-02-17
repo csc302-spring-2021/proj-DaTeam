60c5a8309bc25fabb1adaba3038499c10d9dcb2f
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-02-17 16:21:12 -0500

    Added Basic fomr layout and basic recursive sdcnode processing

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -8,7 +8,7 @@ function RenderNode(sdcnode: SDCNode | null | undefined) {
 
     return (
         <>
-            {sdcnode.id}
+            {/*SDCnode processing for react rendering*/}
             {sdcnode.children.map((childnode, i) => {
                 return (
                     <div key={i} className="mx-4">
@@ -32,27 +32,32 @@ function Form() {
 
     return (
         <div data-testid="form" className="relative">
-            <div data-testid="input-patientid" className="pt-10 pb-3 px-3">
-                <label className="font-bold text-sm"> 
-                    OHIP NUMBER: 
+            <div data-testid="form-title" className="px-8 py-8 text-left text-3xl leading-relaxed">
+                <p className="font-normal">
+                    Respose for <p className="inline font-bold">{form?.title}</p> for <p className="inline font-bold">{"Arnav Verma"}</p>
+                </p>   
+            </div>
+            <div data-testid="input-form-patientid" className="pb-8 px-8">
+                <label className="font-bold text-sm">
+                    OHIP NUMBER:
                 </label>
-                <div>
+                <div className="py-2">
                     <input id="ohipnumber" type="text"
                         placeholder="ex. 1234123123YM"
-                        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md" />
+                        className="px-3 py-3 focus:ring-indigo-500 bg-gray-200 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded" />
                 </div>
             </div>
-            <div data-testid="input-patientname" className="py-3 px-3">
-                <label className="font-bold text-sm"> 
+            <div data-testid="input-form-patientname" className="pb-8 px-8">
+                <label className="font-bold text-sm">
                     PAITIENT NAME:
                 </label>
-                <div>
+                <div className="py-2">
                     <input id="ohipnumber" type="text"
                         placeholder="ex. Arnav Verma"
-                        className="px-3 -mx-2 w-2/5 focus:outline-none focus:ring focus:border-purple-500 border-black rounded-md" />
+                        className="px-3 py-3 focus:ring-indigo-500 bg-gray-200 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded" />
                 </div>
             </div>
-            {RenderNode(form)}
+            {/*RenderNode(form)*/}
         </div>
     );
 }
