735ddf5712e32fe5fbc9439c6be1f01549938c31
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-02-18 01:29:47 -0500

    added service and mock functions

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,65 +1,72 @@
-import { useEffect, useState } from 'react';
-import { buildFormComplete, SDCForm, SDCNode, Patient } from "@dateam/shared"
+import { useEffect, useState } from "react";
+import { SDCNode, Patient } from "@dateam/shared";
+import FormService from "../../services/FormService";
 
 function RenderNode(sdcnode: SDCNode | null | undefined) {
-    if (sdcnode == null || sdcnode === undefined) {
-        return;
-    }
+  if (sdcnode == null || sdcnode === undefined) {
+    return;
+  }
 
-    return (
-        <>
-            {/*SDCnode processing for react rendering*/}
-            {sdcnode.children.map((childnode, i) => {
-                return (
-                    <div key={i} className="mx-4">
-                        {RenderNode(childnode)}
-                    </div>
-                );
-            })}
-        </>
-    );
+  return (
+    <>
+      {/*SDCnode processing for react rendering*/}
+      {sdcnode.children.map((childnode, i) => {
+        return (
+          <div key={i} className="mx-4">
+            {RenderNode(childnode)}
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 function Form() {
-    const [form, setForm] = useState<SDCNode | null | undefined>();
-    const [patient, setPatient] = useState<Patient | null | undefined>();
+  const [form, setForm] = useState<SDCNode | null | undefined>();
+  const [patient, setPatient] = useState<Patient | null | undefined>();
 
-    useEffect(() => {
-        const sdcform: SDCForm = buildFormComplete(); // fetch data
-        setForm(sdcform);
-        console.log(sdcform);
-    }, [])
+  useEffect(() => {
+    FormService.mockRead().then((sdcform) => {
+      setForm(sdcform);
+    });
+  }, []);
 
-    return (
-        <div data-testid="form" className="relative">
-            <div data-testid="form-title" className="px-8 py-8 text-3xl leading-relaxed text-left">
-                <div className="font-normal">
-                    Response for <p className="inline font-bold">{form?.title}</p> for <p className="inline font-bold">{"Arnav Verma"}</p>
-                </div>   
-            </div>
-            <div data-testid="input-form-patientid" className="px-8 pb-8">
-                <label className="text-sm font-bold">
-                    OHIP NUMBER:
-                </label>
-                <div className="py-2">
-                    <input id="ohipnumber" type="text"
-                        placeholder="ex. 1234123123YM"
-                        className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                </div>
-            </div>
-            <div data-testid="input-form-patientname" className="px-8 pb-8">
-                <label className="text-sm font-bold">
-                    PAITIENT NAME:
-                </label>
-                <div className="py-2">
-                    <input id="patientname" type="text"
-                        placeholder="ex. Arnav Verma"
-                        className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                </div>
-            </div>
-            {/*RenderNode(form)*/}
+  return (
+    <div data-testid="form" className="relative">
+      <div
+        data-testid="form-title"
+        className="px-8 py-8 text-3xl leading-relaxed text-left"
+      >
+        <div className="font-normal">
+          Response for <p className="inline font-bold">{form?.title}</p> for{" "}
+          <p className="inline font-bold">{"Arnav Verma"}</p>
         </div>
-    );
+      </div>
+      <div data-testid="input-form-patientid" className="px-8 pb-8">
+        <label className="text-sm font-bold">OHIP NUMBER:</label>
+        <div className="py-2">
+          <input
+            id="ohipnumber"
+            type="text"
+            placeholder="ex. 1234123123YM"
+            className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+      </div>
+      <div data-testid="input-form-patientname" className="px-8 pb-8">
+        <label className="text-sm font-bold">PAITIENT NAME:</label>
+        <div className="py-2">
+          <input
+            id="patientname"
+            type="text"
+            placeholder="ex. Arnav Verma"
+            className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+      </div>
+      {/*RenderNode(form)*/}
+    </div>
+  );
 }
 
 export default Form;
---
+++ frontend/src/services/FormService.ts
@@ -0,0 +1,34 @@
+export default { read, mockRead };
+import { buildFormComplete, SDCForm, SDCNode, Patient } from "@dateam/shared";
+
+/**
+ * Preform a GET request to the /api/v1/form/:formId route
+ *
+ * @param formId An ID for the SDC form that is to be recived.
+ * @returns A SDC Form Object
+ */
+async function read(formId: number): Promise<SDCForm> {
+  try {
+    const formResponse = await fetch(`/api/v1/form/${formId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (formResponse.status != 201) {
+      throw Error(
+        `Could not get form by ID. Error: ${formResponse.statusText}`
+      );
+    }
+    /* TODO: Validate form response json */
+    const form: SDCForm = await formResponse.json();
+    return form;
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function mockRead(): Promise<SDCForm> {
+  const sdcForm: SDCForm = buildFormComplete();
+  return Promise.resolve(sdcForm);
+}
