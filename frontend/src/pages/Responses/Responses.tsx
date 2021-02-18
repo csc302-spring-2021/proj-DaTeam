9ca08c6382d099e123f3be2a51f16d3132db25fc
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-02-17 23:36:42 -0500

    added form states and input forms to responses

--- frontend/src/pages/Responses/Responses.tsx
+++ frontend/src/pages/Responses/Responses.tsx
@@ -1,26 +1,59 @@
-import {Form} from "../../components/Form"
+import { Form } from "../../components/Form";
+import { useState } from "react";
+import { FormInput } from "../../components/FormInput";
 
 function Responses() {
   return (
-    <div data-testid="responses" className="mx-auto px-4">
-    <div className="-mx-4 flex h-screen">
-      <div className="z-20 overflow-y-auto w-full p-4 sm:w-1/2 lg:w-1/4 bg-gray-50 rounded-lg shadow-2xl">
-        <div className=" px-10 py-12 bg-white rounded-lg shadow-lg text-center text-2xl font-sans ">
-            Temporary "Choose a Form" Component
-        </div>
-      </div>
-      <div className="z-10 overflow-y-auto w-full p-4 sm:w-1/2 lg:w-1/4 bg-gray-50 rounded-lg shadow-2xl">
-        <div className="px-10 py-12 bg-white rounded-lg shadow-lg text-center text-2xl font-sans">
-            Temporary "Response" Component
-        </div>
-      </div>
-      <div className="z-0 overflow-y-auto w-full p-4 sm:w-1/2 lg:w-1/2 bg-gray-50 rounded-lg shadow-2xl">
-        <div className="px-10 py-12 bg-white rounded-lg shadow-lg font-sans">
-        <Form/>
+    <div data-testid="responses" className="mx-auto">
+      <div className="flex min-h-screen">
+        <ResponseForms />
+        <FormResponses />
+        <div className="z-0 w-full p-4 overflow-y-auto rounded-lg shadow-2xl sm:w-1/2 lg:w-1/2 bg-gray-50">
+          <div className="px-10 py-12 font-sans bg-white rounded-lg shadow-lg">
+            <Form />
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  );
+}
+
+function ResponseForms() {
+  const [responseFormsSearch, setResponseFormsSearch] = useState("");
+  return (
+    <div className="z-20 w-1/2 w-full px-4 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
+      <h2 className="my-4 ml-4 text-4xl font-semibold"> Choose a Form</h2>
+      <h3 className="mx-2 mb-6 font-medium">
+        {" "}
+        Responses are grouped by the form they come from.
+      </h3>
+      <FormInput
+        placeholder={"Filter by name."}
+        type={"text"}
+        state={responseFormsSearch}
+        setState={setResponseFormsSearch}
+      />
+    </div>
+  );
+}
+
+function FormResponses() {
+  const [formResponseSearch, setFormResponseSearch] = useState("");
+
+  return (
+    <div className="z-10 w-1/2 w-full px-4 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
+      <h2 className="my-4 ml-4 text-4xl font-semibold"> Responses</h2>
+      <h3 className="mx-2 mb-6 font-medium">
+        {" "}
+        Responses are grouped by the form they come from.
+      </h3>
+      <FormInput
+        placeholder={"Filter by name."}
+        type={"text"}
+        state={formResponseSearch}
+        setState={setFormResponseSearch}
+      />
+    </div>
   );
 }
 
