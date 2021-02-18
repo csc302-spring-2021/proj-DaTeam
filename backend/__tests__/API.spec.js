97c767a37dae940c4025bb2b33dfad4c56041343
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-02-18 15:42:26 -0500

    added a mock endpoint test example and updating the scripts to run it

--- backend/__tests__/API.spec.js
+++ backend/__tests__/API.spec.js
@@ -1,10 +1,41 @@
-// Demo API tests 
+const app = require("../build/App").default;
+const http = require("http");
+const supertest = require('supertest');
+
+// Add Mock endpoint
+app.get("/mock", function(req, res) {
+    res.status(200).json({ name: 'dateam' });
+  });
+
+const server = http.createServer(app);
+const request = supertest(server);
+
+
+// Mock Endpoint test
+describe("GET /mock", () => {
+    // For each test case for the endpoint
+    test("Base Case", done => {
+        request
+            .get("/mock")
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err);
+                return done();
+            });
+    });
+});
+
+
+/*
+ * Demo API tests 
+ * Add a new test block for each API end point
+ */
 
 // For each endpoint
-describe("API call 1", () => {
+describe("GET /api/patient/search", () => {
     // For each test case for the endpoint
-    test("case 1", () => {
-        // actual test
-        // Use supertest?
+    test("Base Case", done => {
+        return done();
     });
 });
