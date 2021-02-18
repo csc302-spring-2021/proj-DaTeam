bf8ff80786979af616120927ce975ff46ae5b442
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-02-17 20:22:51 -0500

    Added stub for a demo jest test. Also added an autogenerated file to the .gitignore

---
+++ backend/__tests__/API.spec.js
@@ -0,0 +1,10 @@
+// Demo API tests 
+
+// For each endpoint
+describe("API call 1", () => {
+    // For each test case for the endpoint
+    test("case 1", () => {
+        // actual test
+        // Use supertest?
+    });
+});
