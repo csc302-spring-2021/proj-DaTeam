93e914f21e1e2242d9607ddd4866d0d7d66ea7db
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2021-02-18 15:01:49 -0500

    feat: fix up styles and layout of responses page

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -4,117 +4,97 @@ import { Section } from "../../components/Section";
 import { DisplayItem } from "../../components/DisplayItem";
 import FormService from "../../services/FormService";
 
-
 function RenderNode(sdcnode: SDCNode | null | undefined) {
-    if (sdcnode == null || sdcnode === undefined) {
-        return;
-    }
-    
-    const childNodes: React.ReactNode[] = sdcnode.children.map((childnode, i) => {
-        return (
-            <div key={i}>
-                {RenderNode(childnode)}
-            </div>
-        );
-    });
+  if (sdcnode == null || sdcnode === undefined) {
+    return;
+  }
 
-    let rootNode: React.ReactNode | null = null;
-    switch (sdcnode.class) {
-        case "SDCSection":
-            rootNode = (
-                <Section sdcSection={sdcnode}>
-                    {childNodes}
-                </Section>
-            );
-            break;
-        case "SDCDisplayItem":
-            rootNode = (
-                <DisplayItem sdcDisplayitem={sdcnode}/>
-            );
-            break;
-        case "SDCTextField":
-            rootNode = (
-                <div>
-                    {/*Remove this div and add component*/}
-                    <label data-testid={"question-" + sdcnode.id} >{sdcnode.title}</label>
-                    <div className="py-2">
-                        <input
-                        type="text"
-                        className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        /> 
-                    </div>   
-                    {childNodes}
-                </div>
-            );
-            break;
-        case "SDCListField":
-            rootNode = (
-                <>
-                    {/*Remove this and add component*/}
-                    {childNodes}
-                </>
-            );
-            break;
-        default:
-            rootNode = (
-                <>
-                    {childNodes}
-                </>
-            );
-    }
+  const childNodes: React.ReactNode[] = sdcnode.children.map((childnode, i) => {
+    return <div key={i}>{RenderNode(childnode)}</div>;
+  });
 
-    return (
+  let rootNode: React.ReactNode | null = null;
+  switch (sdcnode.class) {
+    case "SDCSection":
+      rootNode = <Section sdcSection={sdcnode}>{childNodes}</Section>;
+      break;
+    case "SDCDisplayItem":
+      rootNode = <DisplayItem sdcDisplayitem={sdcnode} />;
+      break;
+    case "SDCTextField":
+      rootNode = (
+        <div>
+          {/*Remove this div and add component*/}
+          <label data-testid={"question-" + sdcnode.id}>{sdcnode.title}</label>
+          <div className="py-2">
+            <input
+              type="text"
+              className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          {childNodes}
+        </div>
+      );
+      break;
+    case "SDCListField":
+      rootNode = (
         <>
-            {rootNode}
+          {/*Remove this and add component*/}
+          {childNodes}
         </>
-    );
+      );
+      break;
+    default:
+      rootNode = <>{childNodes}</>;
+  }
+
+  return <>{rootNode}</>;
 }
 
 function Form() {
-    const [form, setForm] = useState<SDCNode | null | undefined>();
-    const [patient, setPatient] = useState<Patient | null | undefined>();
+  const [form, setForm] = useState<SDCNode | null | undefined>();
+  const [patient, setPatient] = useState<Patient | null | undefined>();
 
-    useEffect(() => {
-        FormService.mockRead().then((sdcform) => {
-            setForm(sdcform);
-        });
-    }, []);
+  useEffect(() => {
+    FormService.mockRead().then((sdcform) => {
+      setForm(sdcform);
+    });
+  }, []);
 
-    return (
-        <div data-testid="form" className="relative">
-            <div data-testid="form-title" className="px-8 py-4 text-3xl text-left">
-                <div className="inline-block">
-                    Response of <p className="inline font-bold">{form?.title}</p>
-                </div>
-                <div className="inline-block ml-2">
-                    for <p className="inline font-bold"> {"Arnav Verma"}</p>
-                </div>
-            </div>
-            <div data-testid="input-form-patientid" className="px-8 pb-8">
-                <label className="font-bold">OHIP NUMBER:</label>
-                <div className="py-2">
-                    <input
-                        id="ohipnumber"
-                        type="text"
-                        placeholder="ex. 1234123123YM"
-                        className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-            <div data-testid="input-form-patientname" className="px-8 pb-8">
-                <label className="font-bold">PAITIENT NAME:</label>
-                <div className="py-2">
-                    <input
-                        id="patientname"
-                        type="text"
-                        placeholder="ex. Arnav Verma"
-                        className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-            {RenderNode(form)}
+  return (
+    <div data-testid="form" className="relative p-12 space-y-8">
+      <h2 data-testid="form-title" className="text-3xl tracking-tighter">
+        Response of <span className="font-bold">{form?.title}</span> for{" "}
+        <span className="font-bold"> {"Arnav Verma"}</span>
+      </h2>
+
+      <div data-testid="input-form-patientid">
+        <label className="font-bold">OHIP NUMBER:</label>
+        <div className="py-2">
+          <input
+            id="ohipnumber"
+            type="text"
+            placeholder="ex. 1234123123YM"
+            className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
         </div>
-    );
+      </div>
+
+      <div data-testid="input-form-patientname">
+        <label className="font-bold">PAITIENT NAME:</label>
+        <div className="py-2">
+          <input
+            id="patientname"
+            type="text"
+            placeholder="ex. Arnav Verma"
+            className="block w-full px-3 py-3 bg-gray-200 border-gray-300 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+      </div>
+
+      {RenderNode(form)}
+    </div>
+  );
 }
 
 export default Form;
--- frontend/src/pages/Responses/Responses.tsx
+++ frontend/src/pages/Responses/Responses.tsx
@@ -6,8 +6,8 @@ function Responses() {
   return (
     <div data-testid="responses" className="h-screen mx-auto overflow-hidden">
       <div className="flex h-full">
-        <ResponseForms />
-        <FormResponses />
+        <FormsPanel />
+        <ResponsesPanel />
         <div className="z-0 w-full min-h-full overflow-y-auto rounded-lg shadow-2xl sm:w-1/2 lg:w-1/2 bg-gray-50">
           <Form />
         </div>
@@ -16,7 +16,38 @@ function Responses() {
   );
 }
 
-function ResponseFormInfoBlock({
+function ResponsesCard({
+  responseForm,
+  setSelectedId,
+  selectedId,
+}: {
+  responseForm: { id: number; name: string };
+  setSelectedId: React.Dispatch<React.SetStateAction<number>>;
+  selectedId: number;
+}) {
+  const setSelectedResponseFormId = (id: number) => () => setSelectedId(id);
+  const isSelected = selectedId === responseForm.id;
+  return (
+    <div
+      key={responseForm.id}
+      className={`p-4 cursor-pointer ${
+        isSelected
+          ? "bg-gray-900 text-white"
+          : "text-black bg-gray-50 hover:bg-gray-200"
+      } rounded-lg transition-colors`}
+      onClick={setSelectedResponseFormId(responseForm.id)}
+    >
+      <span
+        className={`text-xs ${isSelected ? "text-gray-400" : "text-gray-500"}`}
+      >
+        ID: {responseForm.id}
+      </span>
+      <h3 className="text-lg font-medium">{responseForm.name}</h3>
+    </div>
+  );
+}
+
+function FormCard({
   responseForm,
   setSelectedId,
   selectedId,
@@ -30,19 +61,25 @@ function ResponseFormInfoBlock({
   return (
     <div
       key={responseForm.id}
-      className={`h-24 py-2 px-4 mx-2 my-4 cursor-pointer ${
+      className={`p-4 cursor-pointer ${
         isSelected
-          ? "bg-black text-white"
+          ? "bg-gray-900 text-white"
           : "text-black bg-gray-50 hover:bg-gray-200"
-      } rounded-lg`}
+      } rounded-lg transition-colors`}
       onClick={setSelectedResponseFormId(responseForm.id)}
     >
-      <div> {responseForm.id} </div>{" "}
+      <span
+        className={`text-xs ${isSelected ? "text-gray-400" : "text-gray-500"}`}
+      >
+        ID: {responseForm.id}
+      </span>
+      <h3 className="text-lg font-medium">{responseForm.name}</h3>
+      <p className="text-sm text-gray-400">X responses</p>
     </div>
   );
 }
 
-function ResponseForms() {
+function FormsPanel() {
   const [responseFormsSearch, setResponseFormsSearch] = useState("");
   const [selectedId, setSelectedId] = useState(1);
 
@@ -56,7 +93,7 @@ function ResponseForms() {
   ];
   const responseFormInfoBlocks = responseFormInfoArr.map((responseForm, i) => {
     return (
-      <ResponseFormInfoBlock
+      <FormCard
         key={i}
         responseForm={responseForm}
         selectedId={selectedId}
@@ -66,36 +103,36 @@ function ResponseForms() {
   });
 
   return (
-    <div className="z-20 w-1/2 w-full px-4 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
-      <h2 className="my-4 ml-2 text-3xl font-semibold"> Choose a Form</h2>
-      <h3 className="mx-2 mb-6 font-medium">
-        Responses are grouped by the form they come from.
-      </h3>
+    <div className="z-20 w-1/2 w-full px-6 py-12 space-y-8 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
+      <div className="space-y-2">
+        <h2 className="text-3xl font-medium tracking-tighter">Choose a Form</h2>
+        <p className="text-gray-600">
+          Responses are grouped by the form they come from.
+        </p>
+      </div>
       <FormInput
-        placeholder={"Filter by name."}
-        type={"text"}
+        placeholder="Filter by name."
+        type="text"
         state={responseFormsSearch}
         setState={setResponseFormsSearch}
       />
-      <div className="h-full">{responseFormInfoBlocks}</div>
+      <div className="space-y-4">{responseFormInfoBlocks}</div>
     </div>
   );
 }
 
-function FormResponses() {
+function ResponsesPanel() {
   const [formResponseSearch, setFormResponseSearch] = useState("");
   const [selectedId, setSelectedId] = useState(2);
   const responseFormInfoArr = [
-    { id: 1, name: "Pancreatic Cancer Biopsy", responses: 25 },
-    { id: 2, name: "Pancreatic Cancer Biopsy", responses: 25 },
-    { id: 3, name: "Pancreatic Cancer Biopsy", responses: 25 },
-    { id: 4, name: "Pancreatic Cancer Biopsy", responses: 25 },
-    { id: 5, name: "Pancreatic Cancer Biopsy", responses: 25 },
-    { id: 6, name: "Pancreatic Cancer Biopsy", responses: 25 },
+    { id: 1, name: "Arnav" },
+    { id: 2, name: "Umar" },
+    { id: 3, name: "Vinay" },
+    { id: 4, name: "Jin" },
   ];
   const responseFormInfoBlocks = responseFormInfoArr.map((responseForm, i) => {
     return (
-      <ResponseFormInfoBlock
+      <ResponsesCard
         key={i}
         responseForm={responseForm}
         selectedId={selectedId}
@@ -103,20 +140,22 @@ function FormResponses() {
       />
     );
   });
+
   return (
-    <div className="z-10 w-1/2 w-full px-4 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
-      <h2 className="my-4 ml-2 text-3xl font-medium"> Responses</h2>
-      <h3 className="mx-2 mb-6 font-medium">
-        {" "}
-        Responses are grouped by the form they come from.
-      </h3>
+    <div className="z-10 w-1/2 w-full px-6 py-12 space-y-8 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
+      <div className="space-y-2">
+        <h2 className="text-3xl font-medium tracking-tighter">Responses</h2>
+        <p className="text-gray-600">
+          Responses are grouped by the form they come from.
+        </p>
+      </div>
       <FormInput
-        placeholder={"Filter by name."}
-        type={"text"}
+        placeholder="Filter by name."
+        type="text"
         state={formResponseSearch}
         setState={setFormResponseSearch}
       />
-      {responseFormInfoBlocks}
+      <div className="space-y-4">{responseFormInfoBlocks}</div>
     </div>
   );
 }
