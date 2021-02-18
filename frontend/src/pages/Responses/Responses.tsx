38112253aa2a6f3cdf065e524476feba79c3dee2
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-02-18 00:47:37 -0500

    added responses and scroll to choose form

--- frontend/src/pages/Responses/Responses.tsx
+++ frontend/src/pages/Responses/Responses.tsx
@@ -4,8 +4,8 @@ import { FormInput } from "../../components/FormInput";
 
 function Responses() {
   return (
-    <div data-testid="responses" className="mx-auto">
-      <div className="flex min-h-screen">
+    <div data-testid="responses" className="h-screen mx-auto overflow-hidden">
+      <div className="flex h-full">
         <ResponseForms />
         <FormResponses />
         <div className="z-0 w-full p-4 overflow-y-auto rounded-lg shadow-2xl sm:w-1/2 lg:w-1/2 bg-gray-50">
@@ -18,11 +18,35 @@ function Responses() {
   );
 }
 
+function ResponseFormInfoBlock({
+  responseForm,
+  setSelectedId,
+  selectedId,
+}: {
+  responseForm: { id: number; name: string; responses: number };
+  setSelectedId: React.Dispatch<React.SetStateAction<number>>;
+  selectedId: number;
+}) {
+  const setSelectedResponseFormId = (id: number) => () => setSelectedId(id);
+  const isSelected = selectedId === responseForm.id;
+  return (
+    <div
+      key={responseForm.id}
+      className={`h-24 py-2 px-4 mx-2 my-4 cursor-pointer ${
+        isSelected
+          ? "bg-black text-white"
+          : "text-black bg-gray-50 hover:bg-gray-200"
+      } rounded-lg`}
+      onClick={setSelectedResponseFormId(responseForm.id)}
+    >
+      <div> {responseForm.id} </div>{" "}
+    </div>
+  );
+}
+
 function ResponseForms() {
   const [responseFormsSearch, setResponseFormsSearch] = useState("");
   const [selectedId, setSelectedId] = useState(1);
-
-  const setSelectedResponseFormId = (id: number) => () => setSelectedId(id);
 
   const responseFormInfoArr = [
     { id: 1, name: "Pancreatic Cancer Biopsy", responses: 25 },
@@ -33,25 +57,19 @@ function ResponseForms() {
     { id: 6, name: "Pancreatic Cancer Biopsy", responses: 25 },
   ];
   const responseFormInfoBlocks = responseFormInfoArr.map((responseForm) => {
-    const isSelected = selectedId === responseForm.id;
     return (
-      <div
-        key={responseForm.id}
-        className={`h-32 mx-2 my-4 cursor-pointer ${
-          isSelected ? "bg-black text-white" : "text-black bg-gray-50 hover:bg-gray-200"
-        } rounded-lg`}
-        onClick={setSelectedResponseFormId(responseForm.id)}
-      >
-        <div> {responseForm.id} </div>{" "}
-      </div>
+      <ResponseFormInfoBlock
+        responseForm={responseForm}
+        selectedId={selectedId}
+        setSelectedId={setSelectedId}
+      />
     );
   });
 
   return (
-    <div className="z-20 w-1/2 w-full px-4 overflow-y-auto rounded-lg shadow-xl lg:w-1/4 bg-gray-50">
+    <div className="z-20 w-1/2 w-full px-4 overflow-y-auto rounded-lg shadow-xl overflow-y lg:w-1/4 bg-gray-50">
       <h2 className="my-4 ml-2 text-3xl font-semibold"> Choose a Form</h2>
       <h3 className="mx-2 mb-6 font-medium">
-        {" "}
         Responses are grouped by the form they come from.
       </h3>
       <FormInput
@@ -60,7 +78,7 @@ function ResponseForms() {
         state={responseFormsSearch}
         setState={setResponseFormsSearch}
       />
-      {responseFormInfoBlocks}
+      <div className="h-full">{responseFormInfoBlocks}</div>
     </div>
   );
 }
