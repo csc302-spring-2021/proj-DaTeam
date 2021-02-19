f8480633890af3bc5bbba94dd52dea42ebda8d2a
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-02-19 17:54:27 -0500

    Revert "chore: shared module export clean up"
    This reverts commit 8c85f83ef62c076cfe20a8eb6625dd99c1435130.

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,7 +1,6 @@
 // Anything exported from this file is available to client packages
-export * as Model from "./ClassDef";
+export * from "./ClassDef";
 export * from "./ClassMeta";
 export * from "./ClassJsonSerializer";
 export * from "./ClassValidator";
-export * as Mocks from "./MockData"
-export { ValidationError, ParsingError } from "./Utils"
+export * from "./MockData"
