8c85f83ef62c076cfe20a8eb6625dd99c1435130
Toby She <dr.tobyshe@gmail.com>
2021-02-19 15:58:26 -0500

    chore: shared module export clean up

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,6 +1,7 @@
 // Anything exported from this file is available to client packages
-export * from "./ClassDef";
+export * as Model from "./ClassDef";
 export * from "./ClassMeta";
 export * from "./ClassJsonSerializer";
 export * from "./ClassValidator";
-export * from "./MockData"
+export * as Mocks from "./MockData"
+export { ValidationError, ParsingError } from "./Utils"
