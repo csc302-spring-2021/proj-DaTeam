b2aaba48390e757826ea262584abf9a56ce36f0f
Toby She <dr.tobyshe@gmail.com>
2021-02-19 19:47:18 -0500

    chore: move TextFieldTypeMeta into a seperate file

--- shared/src/ClassMeta.ts
+++ shared/src/ClassMeta.ts
@@ -3,60 +3,7 @@
  */
 
 import * as Model from "./ClassDef"
-
-class TextFieldTypeMetaType{
-     processor?: (base: string) => string // formate a field, e.g. strip
-     validator?: (field: string) => boolean // validate the field
-}
-
-// TODO
-export const textFieldTypeMeta: { [id: string]: TextFieldTypeMetaType } = {
-     "anyType" : {},
-     "anyURI" : {},
-     "base64Binary" : {},
-     "boolean" : {},
-     "duration" : {},
-     "ID" : {
-          processor: String.prototype.trim
-     },
-     "int" : {
-          processor: String.prototype.trim,
-          validator: new RegExp("^-?[0-9]*$").test
-     },
-     "integer" : {
-          processor: String.prototype.trim,
-          validator: new RegExp("^-?[0-9]*$").test
-     },
-     "byte" : {},
-     "date" : {},
-     "dateTime" : {},
-     "dateTimeStamp" : {},
-     "decimal" : {},
-     "double" : {},
-     "float" : {},
-     "gDay" : {},
-     "gMonth" : {},
-     "gMonthDay" : {},
-     "gYear" : {},
-     "gYearMonth" : {},
-     "hexBinary" : {},
-     "HTML" : {},
-     "long" : {},
-     "negativeInteger" : {},
-     "NMTOKENS" : {},
-     "nonNegativeInteger" : {},
-     "nonPositiveInteger" : {},
-     "positiveInteger" : {},
-     "short" : {},
-     "string" : {},
-     "time" : {},
-     "unsignedByte" : {},
-     "unsignedInt" : {},
-     "unsignedLong" : {},
-     "unsignedShort" : {},
-     "XML" : {},
-     "yearMonthDuration" : {}
-}
+import { textFieldTypeMeta } from "./TextFieldTypeMeta"
 
 class ClassMetaType{
 
