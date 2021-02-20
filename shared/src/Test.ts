fda0a6dfbbbb5811f4a47d719d2bb757ddaeb873
Toby She <dr.tobyshe@gmail.com>
2021-02-19 19:10:06 -0500

    feat: change error type for serializer, added tester

--- shared/src/Test.ts
+++ shared/src/Test.ts
@@ -1,11 +1,19 @@
 import * as Mocks from "./MockData"
 import { GenericClassValidator } from "./ClassValidator"
+import util from "util"
+import { GenericJsonSerializer } from "./ClassJsonSerializer"
+import * as Model from "./ClassDef"
 
 const log = console.log
+const inspect = (o: any) => { log(util.inspect(o, false, null, true)) }
 
 // TODO: this is not the proper way to run tests
 // Need to write using proper testing framework
 
+
+// *** Testing for the validator ***
+
+log("----Validator test----")
 log("----expecting no output----")
 for (let [name, func] of Object.entries(Mocks)){
      log(`Test validation for ${name}`)
@@ -58,3 +66,34 @@ try{
 }catch(e){
      log(e.message)
 }
+
+// *** Testing for the serializer ***
+
+const obj = Mocks.buildFormComplete()
+const encoded = GenericJsonSerializer.encode(obj, Model.SDCForm)
+const decoded = GenericJsonSerializer.decode(encoded, Model.SDCForm)
+
+log("----Serializer test----")
+log("----the object printed below should be the same----")
+
+inspect(obj)
+inspect(decoded)
+
+log("----expecting reasonable error message----")
+
+try{
+     log("Encoding object is unexpected")
+     GenericJsonSerializer.encode(obj, Model.SDCFormResponse)
+}catch(e){
+     log(e.message)
+}
+
+try{
+     log("Decoding object is unexpected")
+     GenericJsonSerializer.decode(encoded, Model.SDCFormResponse)
+}catch(e){
+     log(e.message)
+}
+
+// Since the serializer calls the validator directly
+// No other testing needed
