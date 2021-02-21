50ff6f44c672ae51e4181118b5f253b346c25b23
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-02-21 15:21:29 -0500

    implemented the mock tests for the search patient end point. currently fail because api is not implemented so I still need to test.

--- backend/__tests__/ValidateResponse.ts
+++ backend/__tests__/ValidateResponse.ts
@@ -85,5 +85,81 @@ export default {
           pass: false,
         };
       }
+    },
+    isList(received) {
+      const pass = Array.isArray(received);
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} to not be a list`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to be a list`,
+          pass: false,
+        };
+      }
+    },
+    isListEmpty(received){
+      // PreCondition: received is an array
+      const pass = (received.length == 0);
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} to not be empty`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to be empty`,
+          pass: false,
+        };
+      }
+    },
+    allPatientItems(received) {
+      // PreCondition: received is an array
+      let pass = true;
+      received.forEach(element => {
+        pass = pass && validate(element, Model.Patient);
+      });
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} to not only contain Patient items`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to only contain Patient items`,
+          pass: false,
+        };
+      }
+    },
+    containsPatient(received, patientName, patientId){
+      // PreCondition: received is an array of Patient items
+      let pass = false;
+      received.forEach(element => {
+        let patient = Mock.JsonToObj(element, Model.Patient);
+        if(patient.id == patientId && patient.name == patientName){
+          pass = true;
+        }
+      });
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} to not contain the specified patient`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to contain the specified patient`,
+          pass: false,
+        };
+      }
     }
 }
