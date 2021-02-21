b2791f8ac6fad7c2e85c4ec7b2f003b736b6746f
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-02-20 19:34:10 -0500

    Frontend form (#72)
    * added form controller read
    
    * Co-authored-by: Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
    
    * updated new shared folder

--- frontend/src/components/DisplayItem/DisplayItem.test.tsx
+++ frontend/src/components/DisplayItem/DisplayItem.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import {DisplayItem} from ".";
-import { genDisplayItemPartial } from "@dateam/shared";
+import { Mocks } from "@dateam/shared";
 
 test('render check for display item component', () => {
-    const {getByTestId} = render(<DisplayItem sdcDisplayitem={genDisplayItemPartial()}/>);
+    const {getByTestId} = render(<DisplayItem sdcDisplayitem={Mocks.genDisplayItemPartial()}/>);
     getByTestId("displayitem")
 });
