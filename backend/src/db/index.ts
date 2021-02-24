6e7eaba1a25813c591a93dbcfbd894fdfdb2daa7
Toby She <dr.tobyshe@gmail.com>
2021-02-24 18:12:16 -0500

    feat: skip DB init for CI test

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,5 +1,6 @@
 import { databaseManager } from "./DatabaseManager";
 export function initDB() {
+  if (process.env.CI) return; // skip db init for CI test
   databaseManager.testConnection();
   databaseManager.testQuery();
 }
