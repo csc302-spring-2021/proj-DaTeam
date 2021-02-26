ad56b6cfa97df7bf7f26da30b55f983981cb1fbe
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-02-25 21:37:42 -0500

    implemented the validation tests (converted each existing test from Test.ts)

--- shared/__tests__/Validation.spec.ts
+++ shared/__tests__/Validation.spec.ts
@@ -10,20 +10,37 @@ describe("Mock Data Validator Test", () => {
     }
 });
 
-describe.skip("Verify Invalid Cases", () => {
+describe("Verify Invalid Cases", () => {
+    let form;
+    beforeAll(() => {
+        form = Mocks.buildFormComplete();
+    });
+    function validateForm(){
+        GenericClassValidator.validate(form);
+    }
     test("Root level property invalid" , done => {
+        form.formProperties = 1;
+        expect(validateForm).toThrow();
         done();
     });
     test("Super class property invalid" , done => {
+        form.uid = 1;
+        expect(validateForm).toThrow();
         done();
     });
     test("Sub level missing attribute" , done => {
+        form.children[0].id = null;
+        expect(validateForm).toThrow();
         done();
     });
     test("Child of child is invalid" , done => {
+        form.children[0].children.push({});
+        expect(validateForm).toThrow();
         done();
     });
     test("Attribute validator failed" , done => {
+        form.order = -1;
+        expect(validateForm).toThrow();
         done();
     });
 });
