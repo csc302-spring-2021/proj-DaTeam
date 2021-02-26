aefc9e7b15e1ada0e9ab6498864aceed5321da9c
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-02-25 21:45:37 -0500

    implemented the serializer tests in jest from Tests.ts

--- shared/__tests__/Serializer.spec.ts
+++ shared/__tests__/Serializer.spec.ts
@@ -3,18 +3,26 @@ import * as Model from "../build/ClassDef";
 import { GenericJsonSerializer } from "../build/ClassJsonSerializer";
 
 describe("Basic Serializer Tests", () => {
+    let obj, encoded, decoded;
     beforeAll(() => {
-        const obj = Mocks.buildFormComplete()
-        const encoded = GenericJsonSerializer.encode(obj, Model.SDCForm)
-        const decoded = GenericJsonSerializer.decode(encoded, Model.SDCForm)
+        obj = Mocks.buildFormComplete()
+        encoded = GenericJsonSerializer.encode(obj, Model.SDCForm)
+        decoded = GenericJsonSerializer.decode(encoded, Model.SDCForm)
     });
     test("Encode then decode, expecting no output", done => {
+        expect(obj).toStrictEqual(decoded);
         done();
     });
     test("Encoding object is unexpected", done => {
+        expect(() => {
+            GenericJsonSerializer.encode(obj, Model.SDCFormResponse);
+        }).toThrow();
         done();
     });
     test("Decoding object is unexpected", done => {
+        expect(() => {
+            GenericJsonSerializer.decode(encoded, Model.SDCFormResponse);
+        }).toThrow();
         done();
     });
 });
