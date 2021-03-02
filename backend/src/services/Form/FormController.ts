ade42bd4cb8d5fc5c543b85bf9a78307e20f5aba
Tony (Ze) Yi <24402057+tony-ze-yi@users.noreply.github.com>
2021-03-01 23:48:14 -0500

    feat: initialize fast-xml-parser
    parent 7afefebe4bf52b4fbe8baae41a9089dbcc8e3243
    author Tony (Ze) Yi <24402057+tony-ze-yi@users.noreply.github.com> 1614660494 -0500
    committer Toby She <dr.tobyshe@gmail.com> 1615435161 -0500
    
    feat: initialize fast-xml-parser
    
    feat: implemented POST for parse endpoint
    
    feat: parse function
    
    chore: update dependencies
    
    feat: implemented POST for parse endpoint
    
    feat: parse function
    
    chore: update dependencies
    
    chore: refactor code
    
    fix: changed attribute name prefix
    
    chore: refactor deps
    
    chore: revert lock for merging

--- backend/src/services/Form/FormController.ts
+++ backend/src/services/Form/FormController.ts
@@ -1,18 +1,57 @@
 import { Mocks, GenericJsonSerializer, Model } from "@dateam/shared";
-import { Request, Response } from "express";
+import { json, Request, Response } from "express";
+import { Http } from "winston/lib/winston/transports";
 import { HttpCode } from "../../utils/Error";
 
+var parser = require("fast-xml-parser");
+var he = require("he");
+var options = {
+  attributeNamePrefix: "@_",
+  attrNodeName: "false", //default is 'false'
+  textNodeName: "#text",
+  ignoreAttributes: false,
+  ignoreNameSpace: false,
+  allowBooleanAttributes: false, // boolean i.e. attributes with no value
+  parseNodeValue: true,
+  parseAttributeValue: true,
+  trimValues: true,
+  cdataTagName: "false", //default is 'false'
+  cdataPositionChar: "\\c",
+  parseTrueNumberOnly: false,
+  arrayMode: false, //"strict"
+  attrValueProcessor: (val: string, attrName: string) =>
+    he.decode(val, { isAttributeValue: true }), //default is a=>a
+  tagValueProcessor: (val: string, tagName: string) => he.decode(val), //default is a=>a
+  stopNodes: ["parse-me-as-string"], // tag names that don't need to be parsed, only passed in as a string
+};
+
 export const FormController = {
+  /**
+   * Returns the raw JSON of the parsed xml
+   * @param xmlData: XML string to parse
+   * @returns JSON object representing parsed xml file
+   */
+  parseXML: function (xmlData: string) {
+    if (parser.validate(xmlData) === true) {
+      return parser.parse(xmlData, options);
+    } else {
+      return null;
+    }
+  },
+
   create: function (req: Request, res: Response) {
     res.sendStatus(HttpCode.NOT_IMPLEMENTED);
     //res.status(HttpCode.OK).send();
   },
 
   read: function (req: Request, res: Response) {
-    const sdcForm = Mocks.buildFormComplete();
-    const serialized = GenericJsonSerializer.encode(sdcForm, Model.SDCForm)
-
-    res.status(HttpCode.OK).send(serialized);
+    const sdcForm = this.parseXML(req.body);
+    if (sdcForm === null) {
+      res.status(HttpCode.BAD_REQUEST).send();
+    } else {
+      const serialized = GenericJsonSerializer.encode(sdcForm, Model.SDCForm);
+      res.status(HttpCode.OK).send(serialized);
+    }
   },
 
   update: function (req: Request, res: Response) {
@@ -27,8 +66,8 @@ export const FormController = {
 
   parse: function (req: Request, res: Response) {
     const sdcForm = Mocks.buildFormComplete();
-    const serialized = GenericJsonSerializer.encode(sdcForm, Model.SDCForm)
+    const serialized = GenericJsonSerializer.encode(sdcForm, Model.SDCForm);
 
     res.status(HttpCode.OK).send(serialized);
-  }
+  },
 };
