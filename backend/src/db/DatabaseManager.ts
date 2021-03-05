87ac637ae9fa4587745399fc24582fdf50660d08
Toby She <dr.tobyshe@gmail.com>
2021-03-05 17:30:13 -0500

    feat: allow db connection via url

--- backend/src/db/DatabaseManager.ts
+++ backend/src/db/DatabaseManager.ts
@@ -22,6 +22,7 @@ class DatabaseManager {
       database: process.env.DB_DATABASE || "",
       user: process.env.DB_USER || "",
       password: process.env.DB_PASSWORD || "",
+      connectionString: process.env.DATABASE_URL || ""
     };
 
     if (process.env.NODE_ENV === "development") {
