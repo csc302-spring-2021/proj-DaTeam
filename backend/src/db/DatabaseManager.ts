405bbb3c64cd412fcf59b58bca1cf61e61bec7c8
Toby She <dr.tobyshe@gmail.com>
2021-03-05 17:43:44 -0500

    feat: added database connection option ssl

--- backend/src/db/DatabaseManager.ts
+++ backend/src/db/DatabaseManager.ts
@@ -22,7 +22,8 @@ class DatabaseManager {
       database: process.env.DB_DATABASE || "",
       user: process.env.DB_USER || "",
       password: process.env.DB_PASSWORD || "",
-      connectionString: process.env.DATABASE_URL || ""
+      connectionString: process.env.DATABASE_URL || "",
+      ssl: process.env.DB_SSL == "true"
     };
 
     if (process.env.NODE_ENV === "development") {
