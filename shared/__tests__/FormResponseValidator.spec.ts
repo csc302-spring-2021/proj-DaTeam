d09d9c5a44cd66fe0c023fca181a471f8df5d3e1
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2021-03-06 16:15:41 -0500

    Undid my changes for FormResponseValidator; specifically my change from adding error to list instead of throwing. Return to original since maybe there is business logic/a reason it is like that. Additionally, refactored my existing tests so to check the returned errors to see if it contains the correct one

--- shared/__tests__/FormResponseValidator.spec.ts
+++ shared/__tests__/FormResponseValidator.spec.ts
@@ -2,18 +2,48 @@ import {FormResponseValidator} from "../build/FormResponseValidator";
 import * as Mocks from "../build/MockData";
 import {ValidationError} from "../build/Utils";
 
+beforeAll(() => {
+    expect.extend({
+        containsError(received, errorTxt) {
+            let pass = false;
+            let messages = received.map(err => err.message);
+            for(let msg_i in messages){
+                let msg = messages[msg_i];
+                if(msg === errorTxt){
+                    pass = true;
+                    break;
+                }
+            }
+            if (pass) {
+              return {
+                message: () =>
+                  `expected ${received} to not contain error with text ${errorTxt}`,
+                pass: true,
+              };
+            } else {
+              return {
+                message: () =>
+                  `expected ${received} to contain error with text ${errorTxt}`,
+                pass: false,
+              };
+            }
+          }
+    });
+});
+
 describe("Verify Form Response Validator", () => {
     function validateFormResponse(){
         FormResponseValidator.validate(Mocks.buildFormResponseComplete(), Mocks.buildFormComplete());
     }
-    test("Basic Case: Mock Data", done => {
+    test("Basic Case: Default Mock Data Passes", done => {
         expect(validateFormResponse).not.toThrow();
         done();
     });
 });
 
 describe("Verify TextField Failures", () => {
-    let form, formResponse;
+    let form, formResponse, errors;
+    ;
     beforeAll(() => {
         form = Mocks.buildFormComplete();
     });
@@ -21,7 +51,7 @@ describe("Verify TextField Failures", () => {
         formResponse = Mocks.buildFormResponseComplete();
     });
     function validateFormResponse(){
-        FormResponseValidator.validate(formResponse, form);
+        errors = FormResponseValidator.validate(formResponse, form);
     }
     test("Multiple Responses Throws Validation Error", done => {
         for(let answer of formResponse.answers){
@@ -33,24 +63,25 @@ describe("Verify TextField Failures", () => {
         expect(validateFormResponse).toThrowError(ValidationError);
         done();
     });
-    test("No Responses Throws Validation Error", done => {
+    test("Empty Responses Returns Validation Error", done => {
         for(let answer of formResponse.answers){
             if("text-1" === answer.questionID){
                 answer.responses = [];
                 break;
             }
         }
-        expect(validateFormResponse).toThrowError(ValidationError);
+        validateFormResponse();
+        expect(errors).containsError('Question must be answered');
         done();
     });
-    test("Invalid Response Throws Validation Error", done => {
+    test("Invalid Response (non-int) Returns Validation Error", done => {
         for(let answer of formResponse.answers){
             if("text-1" === answer.questionID){
                 answer.responses = ["this should throw an error"];
                 break;
             }
         }
-        expect(validateFormResponse).toThrowError(ValidationError);
+        expect(errors).containsError('Input is not a valid int');
         done();
     });
 });
