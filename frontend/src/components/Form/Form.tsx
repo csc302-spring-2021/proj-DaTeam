dcc1caa099ecdb686c92b05472a4d4fd82f51cd4
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-03-10 01:54:33 -0500

    removed form recursion

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -11,123 +11,154 @@ import { ListFieldItem } from "../../components/ListFieldItem";
 import { createLogicalOr, isPropertySignature } from "typescript";
 
 interface IMockPrivateClass {
-    __class?: "SDCSection" | "SDCDisplayItem" | "SDCTextField" | "SDCListField";
+  __class?: "SDCSection" | "SDCDisplayItem" | "SDCTextField" | "SDCListField";
 }
-
-
 
 function Form() {
-    const [sdcform, setSdcform] = useState<Model.SDCNode | undefined>(undefined);
-    const [patient, setPatient] = useState<Model.Patient | undefined>(undefined);
-    const [response, setResponse] = useState<{ [key: string]: any; }>({});
+  const [sdcform, setSdcform] = useState<Model.SDCNode | undefined>(undefined);
+  const [patient, setPatient] = useState<Model.Patient | undefined>(undefined);
+  const [response, setResponse] = useState<{ [key: string]: any }>({});
 
-    function RenderNode(sdcnode: Model.SDCNode & IMockPrivateClass): ReactNode {
-        const childNodes = sdcnode.children.map((childnode) => {
-            return <div key={childnode.id}>{RenderNode(childnode)}</div>;
+  function RenderNode(sdcnode: Model.SDCNode & IMockPrivateClass): ReactNode {
+    const childNodes = sdcnode.children.map((childnode) => {
+      return <div key={childnode.id}>{RenderNode(childnode)}</div>;
+    });
+
+    if (sdcnode instanceof Model.SDCSection) {
+      return (
+        <Section sdcSection={sdcnode as Model.SDCSection}>{childNodes}</Section>
+      );
+    } else if (sdcnode instanceof Model.SDCDisplayItem) {
+      return <DisplayItem sdcDisplayitem={sdcnode as Model.SDCDisplayItem} />;
+    } else if (sdcnode instanceof Model.SDCTextField) {
+      return (
+        <>
+          <TextField
+            responseState={{ response, setResponse }}
+            sdcTextField={sdcnode}
+          >
+            {childNodes}
+          </TextField>
+        </>
+      );
+    } else if (sdcnode instanceof Model.SDCListField) {
+      const optionsNodes: {
+        listFieldItem: Model.SDCListFieldItem;
+        listFieldItemChildren: JSX.Element[];
+      }[] = sdcnode.options.map((optionnode: Model.SDCListFieldItem) => {
+        const optionChild = optionnode.children.map((childnode) => {
+          return <div key={childnode.id}>{RenderNode(childnode)}</div>;
         });
-
-        if (sdcnode instanceof Model.SDCSection) {
-            return (
-                <Section sdcSection={sdcnode as Model.SDCSection}>{childNodes}</Section>
-            );
-        } else if (sdcnode instanceof Model.SDCDisplayItem) {
-            return <DisplayItem sdcDisplayitem={sdcnode as Model.SDCDisplayItem} />;
-        } else if (sdcnode instanceof Model.SDCTextField) {
-            return <><TextField responseState={{ response, setResponse }} sdcTextField={sdcnode}>{childNodes}</TextField></>
-        } else if (sdcnode instanceof Model.SDCListField) {
-            const optionsNodes: { listFieldItem: Model.SDCListFieldItem; listFieldItemChildren: JSX.Element[] }[] = sdcnode.options.map((optionnode: Model.SDCListFieldItem) => {
-                const optionChild = optionnode.children.map((childnode) => {
-                    return <div key={childnode.id}>{RenderNode(childnode)}</div>;
-                });
-                return { listFieldItem: optionnode, listFieldItemChildren: optionChild };
-            });
-            return <><ListField responseState={{ response, setResponse }} sdcListField={sdcnode} optionNodes={optionsNodes}>{childNodes}</ListField></>;
-        } else {
-            return <>{childNodes}</>;
-        }
+        return {
+          listFieldItem: optionnode,
+          listFieldItemChildren: optionChild,
+        };
+      });
+      return (
+        <>
+          <ListField
+            responseState={{ response, setResponse }}
+            sdcListField={sdcnode}
+            optionNodes={optionsNodes}
+          >
+            {childNodes}
+          </ListField>
+        </>
+      );
+    } else {
+      return <>{childNodes}</>;
     }
+  }
 
-    const onSubmitForm = () => {
-        console.log(response);
-    }
+  const onSubmitForm = () => {
+    console.log(response);
+  };
 
-    useEffect(() => {
-        FormService.read(123)
-            .then((sdcform) => {
-                const decodedSdcNode = GenericJsonSerializer.decode(
-                    sdcform,
-                    Model.SDCNode
-                );
-                setSdcform(decodedSdcNode);
-                /* Remove to use actual patient */
-                const patient = new Model.Patient({});
-                patient.id = "111";
-                patient.name = "Arnav Verma";
-                patient.uid = "testuuid";
-                setPatient(patient);
-            })
-            .catch((err) => notify.error(err.message));
-    }, []);
+  useEffect(() => {
+    FormService.read(123)
+      .then((sdcform) => {
+        const decodedSdcNode = GenericJsonSerializer.decode(
+          sdcform,
+          Model.SDCNode
+        );
+        setSdcform(decodedSdcNode);
+        /* Remove to use actual patient */
+        const patient = new Model.Patient({});
+        patient.id = "111";
+        patient.name = "Arnav Verma";
+        patient.uid = "testuuid";
+        setPatient(patient);
+      })
+      .catch((err) => notify.error(err.message));
+  }, []);
 
-    if (!sdcform || !patient) {
-        return <NotFound />;
-    }
+  if (!sdcform || !patient) {
+    return <NotFound />;
+  }
 
-    return (
-        <div data-testid="form" className="p-12 space-y-8">
-            <h2 data-testid="form-title" className="text-3xl tracking-tighter">
-                Response of <span className="font-bold">{sdcform?.title}</span> for{" "}
-                <span className="font-bold"> {patient.name}</span>
-            </h2>
-            <div className="flex flex-col justify-between space-x-4 md:flex-row">
-                <div className="w-1/2" data-testid="input-form-patientid">
-                    <ValueBlock id="patientid" value={sdcform.id} label="OHIP number" />
-                </div>
-                <div className="w-1/2" data-testid="input-form-patientname">
-                    <ValueBlock
-                        id="patientname"
-                        value={patient.name}
-                        label="Patient Name"
-                    />
-                </div>
-            </div>
-            {RenderNode(sdcform)}
-            <div className="flex flex-row-reverse">
-                <button onClick={onSubmitForm} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                    <span>Submit </span>
-                    <svg className="fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                    </svg>
-                </button>
-            </div>
+  return (
+    <div data-testid="form" className="p-12 space-y-8">
+      <h2 data-testid="form-title" className="text-3xl tracking-tighter">
+        Response of <span className="font-bold">{sdcform?.title}</span> for{" "}
+        <span className="font-bold"> {patient.name}</span>
+      </h2>
+      <div className="flex flex-col justify-between space-x-4 md:flex-row">
+        <div className="w-1/2" data-testid="input-form-patientid">
+          <ValueBlock id="patientid" value={sdcform.id} label="OHIP number" />
         </div>
-    );
+        <div className="w-1/2" data-testid="input-form-patientname">
+          <ValueBlock
+            id="patientname"
+            value={patient.name}
+            label="Patient Name"
+          />
+        </div>
+      </div>
+      {RenderNode(sdcform)}
+      <div className="flex flex-row-reverse">
+        <button
+          onClick={onSubmitForm}
+          className="inline-flex items-center px-4 py-2 font-bold text-gray-800 bg-gray-300 rounded hover:bg-gray-400"
+        >
+          <span>Submit </span>
+          <svg
+            className="w-8 h-8 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
 }
-
 
 export default Form;
 
-
 function ValueBlock({
-    value,
-    id,
-    label,
+  value,
+  id,
+  label,
 }: {
-    value: string;
-    id: string;
-    label: string;
+  value: string;
+  id: string;
+  label: string;
 }) {
-    return (
-        <>
-            <label className="text-lg font-bold tracking-wide">
-                {label}:
-      </label>
-            <div
-                id={id}
-                className="block w-full px-3 py-2 my-1 text-xl bg-gray-200 border-gray-300 rounded"
-            >
-                {value}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <label className="text-lg font-bold tracking-wide">{label}:</label>
+      <div
+        id={id}
+        className="block w-full px-3 py-2 my-1 text-xl bg-gray-200 border-gray-300 rounded"
+      >
+        {value}
+      </div>
+    </>
+  );
 }
--- frontend/src/components/ListField/ListField.tsx
+++ frontend/src/components/ListField/ListField.tsx
@@ -3,67 +3,100 @@ import { useEffect, useState } from "react";
 import { preProcessFile } from "typescript";
 import { ListFieldItem } from "../ListFieldItem";
 
+function ListField(props: {
+  responseState: {
+    setResponse: React.Dispatch<React.SetStateAction<{ [key: string]: any }>>;
+    response: { [key: string]: any };
+  };
+  optionNodes: {
+    listFieldItem: Model.SDCListFieldItem;
+    listFieldItemChildren: JSX.Element[];
+  }[];
+  children?: any;
+  sdcListField: Model.SDCListField;
+}) {
+  const [numSelected, setNumSelected] = useState<number>(0);
+  const [isMultiSelect, setIsMultiSelect] = useState<boolean>(false);
 
-function ListField(props: { responseState: { setResponse: React.Dispatch<React.SetStateAction<{ [key: string]: any; }>>; response: { [key: string]: any; } }; optionNodes: { listFieldItem: Model.SDCListFieldItem; listFieldItemChildren: JSX.Element[] }[]; children?: any; sdcListField: Model.SDCListField }) {
-    const [numSelected, setNumSelected] = useState<number>(0);
-    const [isMultiSelect, setIsMultiSelect] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (props.sdcListField.maxSelections !== 1) {
-            setIsMultiSelect(true);
-        }
-    }, [props.optionNodes]);
-
-
-
-    const onChangeSelect = (event: any) => {
-        event.stopPropagation();
-        const response: { [key: string]: any; } = { ...props.responseState.response };
-        if(numSelected === 0){
-            setNumSelected(1);
-        }
-        response[props.sdcListField.id] = [event.target.id];
-        props.responseState.setResponse(response);
+  useEffect(() => {
+    if (props.sdcListField.maxSelections !== 1) {
+      setIsMultiSelect(true);
     }
+  }, [props.optionNodes]);
 
-    const onChangeMultiSelect = (event: any) => {
-        event.stopPropagation();
-        if (event.target.checked && numSelected >= props.sdcListField.maxSelections + 5) {
-            event.target.checked = false;
-        } else {
-            const response: { [key: string]: any; } = { ...props.responseState.response };
-            if(response[props.sdcListField.id] === undefined){
-                response[props.sdcListField.id] = [];
-                
-            }
-            let index = response[props.sdcListField.id].indexOf(event.target.id);
-            if (event.target.checked && index === -1) {
-                response[props.sdcListField.id].push(event.target.id)
-                setNumSelected(numSelected + 1);
-            }else if(!event.target.checked && index !== -1){
-                response[props.sdcListField.id].splice(index, 1);
-                setNumSelected(numSelected - 1);
-            }
-            props.responseState.setResponse(response);
-        }
+  const onChangeSelect = (event: any) => {
+    event.stopPropagation();
+    const response: { [key: string]: any } = {
+      ...props.responseState.response,
+    };
+    if (numSelected === 0) {
+      setNumSelected(1);
     }
+    response[props.sdcListField.id] = [event.target.id];
+    props.responseState.setResponse(response);
+  };
 
-    return (
-        <div
-            data-testid="listfield"
-            className="py-2 text-lg font-bold tracking-wide"
-        >
-            <div className="px-2">
-                {props.sdcListField.id}
-            </div>
-            <form onChange={isMultiSelect ? onChangeMultiSelect : onChangeSelect}>
-                {props.optionNodes.map((optionnode: { listFieldItem: Model.SDCListFieldItem; listFieldItemChildren: any[] }) => {
-                    return <ListFieldItem key={optionnode.listFieldItem.id} responseState={props.responseState} isMultiSelect={isMultiSelect} isSelected={props.responseState.response[props.sdcListField.id] ? props.responseState.response[props.sdcListField.id].indexOf(optionnode.listFieldItem.id) !== -1 : false} sdcListFieldItem={optionnode.listFieldItem}>{optionnode.listFieldItemChildren}</ListFieldItem>
-                })}
-            </form>
-            {props.children}
-        </div>
-    );
+  const onChangeMultiSelect = (event: any) => {
+    event.stopPropagation();
+    if (
+      event.target.checked &&
+      numSelected >= props.sdcListField.maxSelections + 5
+    ) {
+      event.target.checked = false;
+    } else {
+      const response: { [key: string]: any } = {
+        ...props.responseState.response,
+      };
+      if (response[props.sdcListField.id] === undefined) {
+        response[props.sdcListField.id] = [];
+      }
+      let index = response[props.sdcListField.id].indexOf(event.target.id);
+      if (event.target.checked && index === -1) {
+        response[props.sdcListField.id].push(event.target.id);
+        setNumSelected(numSelected + 1);
+      } else if (!event.target.checked && index !== -1) {
+        response[props.sdcListField.id].splice(index, 1);
+        setNumSelected(numSelected - 1);
+      }
+      props.responseState.setResponse(response);
+    }
+  };
+
+  return (
+    <div
+      data-testid="listfield"
+      className="py-2 text-lg font-bold tracking-wide"
+    >
+      <div className="px-2">{props.sdcListField.id}</div>
+      <div onChange={isMultiSelect ? onChangeMultiSelect : onChangeSelect}>
+        {props.optionNodes.map(
+          (optionnode: {
+            listFieldItem: Model.SDCListFieldItem;
+            listFieldItemChildren: any[];
+          }) => {
+            return (
+              <ListFieldItem
+                key={optionnode.listFieldItem.id}
+                responseState={props.responseState}
+                isMultiSelect={isMultiSelect}
+                isSelected={
+                  props.responseState.response[props.sdcListField.id]
+                    ? props.responseState.response[
+                        props.sdcListField.id
+                      ].indexOf(optionnode.listFieldItem.id) !== -1
+                    : false
+                }
+                sdcListFieldItem={optionnode.listFieldItem}
+              >
+                {optionnode.listFieldItemChildren}
+              </ListFieldItem>
+            );
+          }
+        )}
+      </div>
+      {props.children}
+    </div>
+  );
 }
 
 export default ListField;
