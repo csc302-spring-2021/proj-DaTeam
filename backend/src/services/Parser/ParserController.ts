9aa1acdc3a907586aef952564a4356e039d0aeae
Toby She <dr.tobyshe@gmail.com>
2021-03-20 18:09:05 -0400

    feat: parser API handles errors properly

--- backend/src/services/Parser/ParserController.ts
+++ backend/src/services/Parser/ParserController.ts
@@ -1,14 +1,16 @@
-import { Mocks, GenericJsonSerializer, Model } from "@dateam/shared";
+import { GenericJsonSerializer, Model } from "@dateam/shared";
 import { Request, Response } from "express";
-import { HttpCode } from "../../utils/Error";
+import { HttpCode, sendError } from "../../utils/Error";
 import { sdcParser } from "../../SDCParser";
 
 export const ParseController = {
   parse: function (req: Request, res: Response) {
-    console.log(req.body.toString());
-    const form = sdcParser.xmlToSDCForm(req.body.toString());
-    const serialized = GenericJsonSerializer.encode(form, Model.SDCForm);
-
-    res.status(HttpCode.OK).send(serialized);
+    try {
+      const form = sdcParser.xmlToSDCForm(req.body.toString());
+      const serialized = GenericJsonSerializer.encode(form, Model.SDCForm);
+      res.status(HttpCode.OK).send(serialized);
+    } catch (e) {
+      sendError(res, HttpCode.BAD_REQUEST, e);
+    }
   },
 };
