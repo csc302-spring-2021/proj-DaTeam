8f4dc7b50e4400bfd28684982af01ec94f2b2657
Umar Ahmed <umar.ahmed@mail.utoronto.ca>
2021-03-21 18:20:52 -0400

    wip: create react hooks for data fetching

---
+++ frontend/src/hooks/services.ts
@@ -0,0 +1,55 @@
+import { useQuery } from "react-query";
+import FormService from "../services/FormService";
+import PatientService from "../services/PatientService";
+import ResponseService from "../services/ResponseService";
+
+export function usePatient(id: string) {
+  const { isLoading, error, data } = useQuery(
+    ["patients", id],
+    () => PatientService.read(id),
+    {
+      enabled: Boolean(id),
+    }
+  );
+  return { isLoading, error, data };
+}
+
+export function useForms() {
+  const { isLoading, error, data } = useQuery("forms", () =>
+    FormService.list()
+  );
+  return { isLoading, error, data };
+}
+
+export function useForm(id: string) {
+  const { isLoading, error, data } = useQuery(
+    ["forms", id],
+    () => FormService.read(id),
+    {
+      enabled: Boolean(id),
+    }
+  );
+  return { isLoading, error, data };
+}
+
+export function useFormResponses(formId: string) {
+  const { isLoading, error, data } = useQuery(
+    ["forms", formId, "responses"],
+    () => ResponseService.list(formId),
+    {
+      enabled: Boolean(formId),
+    }
+  );
+  return { isLoading, error, data };
+}
+
+export function useFormResponse(responseId: string) {
+  const { isLoading, error, data } = useQuery(
+    ["responses", responseId],
+    () => ResponseService.read(responseId),
+    {
+      enabled: Boolean(responseId),
+    }
+  );
+  return { isLoading, error, data };
+}
