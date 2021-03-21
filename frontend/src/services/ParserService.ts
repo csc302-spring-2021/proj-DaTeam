7ce0aa15ac00b0187f27e64109e7d1e173d5de3a
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-03-21 00:50:47 -0400

    updated parser service

--- frontend/src/services/ParserService.ts
+++ frontend/src/services/ParserService.ts
@@ -1,14 +1,15 @@
-import { Model } from "@dateam/shared";
-
 /**
- * Preform a POST request to the /api/v1/parser route
+ * Preform a POST request to the /api/v1/forms/parser route
  * This converts an XML file to a JSON parsed object
  * @param file
  * 
  */
 async function parse(file: any): Promise<void> {
-    const parserResponse = await fetch(`/api/v1/patients`, {
+    const parserResponse = await fetch(`/api/v1/forms/parse`, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/xml"
+      },
       body: file,
     });
     if (parserResponse.status != 200) {
