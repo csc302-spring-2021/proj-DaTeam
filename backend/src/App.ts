1436cd3a67f46030588357347538eb2a29da6d90
Toby She <dr.tobyshe@gmail.com>
2021-03-23 13:50:28 -0400

    fix: increase json post limit

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -10,7 +10,7 @@ import { HttpCode } from "./utils/Error";
 import { ServicesAPI } from "./services";
 import { databaseManager } from "./db";
 
-if (!process.env.CI) databaseManager.testConnection()
+if (!process.env.CI) databaseManager.testConnection();
 
 const app = express();
 
@@ -18,7 +18,7 @@ const app = express();
 // block cross origin access
 app.use(cors());
 // convert all incoming requests to json
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // Serve OpenAPI documentation
