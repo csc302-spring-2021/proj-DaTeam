a2eaccddd2885f3740fd0826a19623284355598f
Ensar Ilyas <ensar.ilyas@mail.utoronto.ca>
2021-04-03 21:28:39 -0400

    Added FormResponse validation to ResponseController:create

--- backend/src/services/Response/ResponseController.ts
+++ backend/src/services/Response/ResponseController.ts
@@ -1,13 +1,26 @@
-import { Model } from "@dateam/shared";
+import { Model, FormResponseValidator, ValidationError } from "@dateam/shared";
 import { Request, Response } from "express";
 import { HttpCode, sendError } from "../../utils/Error";
 import * as Utils from "../ControllerUtils";
+import { databaseManager as dbManager } from "../../db";
 
 export const ResponseController = {
   create: function (req: Request, res: Response) {
-    Utils.create(req, res, Model.SDCFormResponse).catch((e) =>
-      sendError(res, HttpCode.BAD_REQUEST, e)
-    );
+    dbManager
+      .genericRead(req.body.formId, Model.SDCForm)
+      .then((form: Model.SDCForm) => {
+        const validation = FormResponseValidator.validate(req.body, form);
+        if (validation.length == 0) {
+          Utils.create(req, res, Model.SDCFormResponse).catch((e: Error) =>
+            sendError(res, HttpCode.BAD_REQUEST, e)
+          );
+        } else {
+          sendError(res, HttpCode.BAD_REQUEST, Error("Invalid FormResponse"));
+        }
+      })
+      .catch((e: Error) => {
+        sendError(res, HttpCode.NOT_FOUND, e);
+      });
   },
 
   read: function (req: Request, res: Response) {
