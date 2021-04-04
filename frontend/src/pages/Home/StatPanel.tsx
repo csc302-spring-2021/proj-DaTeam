1b1dcd648620f5df5fdda673fd64d72522f0640f
Arnav Verma <arnav.verma@mail.utoronto.ca>
2021-04-04 13:25:24 -0400

    added changes

--- frontend/src/pages/Home/StatPanel.tsx
+++ frontend/src/pages/Home/StatPanel.tsx
@@ -7,6 +7,7 @@ import ResponseService from "../../services/ResponseService";
 import { useRef } from "react";
 import { Model } from "@dateam/shared";
 import { sunburst } from "./Sunburst";
+import * as d3 from "d3";
 
 function StatPanel() {
   const { formId } = useParams<{
@@ -17,92 +18,80 @@ function StatPanel() {
   const { data: formResponses } = useFormResponses(formId);
   const [data, setData] = useState<any>({});
   const [chartData, setChartData] = useState<any>();
+  const [isSet, setIsSet] = useState(false);
   const [idMap, setIdMap] = useState<object>();
   const chartRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
-    let obj: object = {};
-    const formIdMap = (node: Model.SDCNode, obj: any) => {
-      const id = node.id;
-      const title = node.title ? node.title : "ID: " + node.id;
-      obj[id] = title;
-      if (node.children) {
-        node.children.forEach((cnode) => formIdMap(cnode, obj));
-      }
-      if (node instanceof Model.SDCListField) {
-        node.options.forEach((onode) => formIdMap(onode, obj));
-      }
-    };
-    if (formTemplate) {
-      formIdMap(formTemplate, obj);
-      setIdMap(obj);
-      console.log(obj);
-    }
-  }, [formTemplate]);
-
-  useEffect(() => {
-    if (!formResponses) {
-      return;
-    }
-    const responses = formResponses.map(async (res) => {
-      return await ResponseService.read(res.uid!);
-    });
-    Promise.all(responses)
-      .then((resps) => {
-        console.log(resps);
-        const ansIdData = {} as any;
-        resps.forEach((res) => {
-          res.answers.forEach((ans) => {
-            ansIdData[ans.questionID] = [...ans.responses].concat(
-              ansIdData[ans.questionID] ? ansIdData[ans.questionID] : "No Answer"
-            );
+    if (formResponses && formTemplate && chartRef.current) {
+      d3.select(chartRef.current).selectAll("*").remove();
+      const responses = formResponses.map(async (res) => {
+        return await ResponseService.read(res.uid!);
+      });
+      Promise.all(responses)
+        .then((resps) => {
+          const ansIdData = {} as any;
+          resps.forEach((res) => {
+            res.answers.forEach((ans) => {
+              ansIdData[ans.questionID] = [...ans.responses].concat(
+                ansIdData[ans.questionID]
+                  ? ansIdData[ans.questionID]
+                  : "No Answer"
+              );
+            });
           });
-        });
+          return ansIdData;
+        })
+        .then((data) => {
+          if (!formTemplate) {
+            return;
+          }
+          const dataArr = Object.keys(data).map((key: string) => {
+            const countedChildren: any = {};
+            data[key].forEach((key: string) => {
+              countedChildren[key] = countedChildren[key]
+                ? countedChildren[key] + 1
+                : 1;
+            });
+            const children = Object.keys(countedChildren).map(
+              (child: string) => {
+                return { name: child, value: countedChildren[child] };
+              }
+            );
+            return { name: key, children };
+          });
 
-        return ansIdData;
-      })
-      .then((ansIdData) => {
-        setData(ansIdData);
-      });
-  }, [formResponses]);
+          const chartData = { name: formTemplate.title, children: dataArr };
+          return chartData;
+        })
+        .then((chartData) => {
+          let obj: object = {};
+          const formIdMap = (node: Model.SDCNode, obj: any) => {
+            const id = node.id;
+            const title = node.title ? node.title : "ID: " + node.id;
+            obj[id] = title;
+            if (node.children) {
+              node.children.forEach((cnode) => formIdMap(cnode, obj));
+            }
+            if (node instanceof Model.SDCListField) {
+              node.options.forEach((onode) => formIdMap(onode, obj));
+            }
+          };
 
-  useEffect(() => {
-    if (data && formTemplate) {
-      const dataArr = Object.keys(data).map((key: string) => {
-        const countedChildren: any = {};
-        data[key].forEach((key: string) => {
-          countedChildren[key] = countedChildren[key]
-            ? countedChildren[key] + 1
-            : 1;
-        });
-        const children = Object.keys(countedChildren).map((child: string) => {
-          return { name: child, value: countedChildren[child] };
-        });
-        return { name: key, children };
-      });
+          formIdMap(formTemplate, obj);
+          return { chartData, idMap: obj };
+        })
+        .then((info) => {
+          sunburst(info.chartData, chartRef.current, info.idMap);
 
-      const chartData = { name: formTemplate.title, children: dataArr };
-      setChartData(chartData);
+          console.log(formResponses, formTemplate, chartRef);
+        });
     }
-  }, [data, chartRef, formTemplate]);
-
-  useEffect(() => {
-    if (
-      idMap &&
-      chartRef.current &&
-      chartData &&
-      chartData.children.length > 0
-    ) {
-      sunburst(chartData, chartRef.current, idMap);
-    }
-  }, [chartData, chartRef, idMap]);
+  }, [formResponses, formTemplate, chartRef]);
 
   return (
-    <div key={formId} className="w-full h-full py-8 overflow-scroll">
-      <svg
-        className="flex w-full h-full overflow-scroll font-bold"
-        ref={chartRef}
-      />
+    <div className="w-3/4 h-full p-8 ">
+      <svg className="flex w-full h-full font-bold" ref={chartRef} />
     </div>
   );
 }
