d878c8328286d8c84e278d7e7ecf550ed4a99be9
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-04-04 17:33:17 -0400

    Fix home tests

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,27 +6,28 @@ import FormsPanel from "../../components/FormsPanel/FormsPanel";
 import StatPanel from "./StatPanel";
 
 export default function Home() {
-  return (
-    <motion.div
-      variants={pageVariants}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      data-testid="home"
-      className="h-screen mx-auto overflow-hidden"
-    >
-      <div className="flex h-full">
-        <Route
-          exact
-          path={["/home", "/home/:formId"]}
-          component={FormsHomePanel}
-        />
-        <Route exact path={["/home/:formId"]} component={StatPanel} />
-      </div>
-    </motion.div>
-  );
+    return (
+        <div data-testid="home"
+        ><motion.div
+            variants={pageVariants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="h-screen mx-auto overflow-hidden"
+        >
+                <div className="flex h-full">
+                    <Route
+                        exact
+                        path={["/home", "/home/:formId"]}
+                        component={FormsHomePanel}
+                    />
+                    <Route exact path={["/home/:formId"]} component={StatPanel} />
+                </div>
+            </motion.div>
+        </div>
+    );
 }
 
 function FormsHomePanel() {
-  return <FormsPanel baseUri="/home" />;
+    return <FormsPanel baseUri="/home" />;
 }
