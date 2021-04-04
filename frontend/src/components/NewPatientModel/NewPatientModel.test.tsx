8b3878732fcf96b9eadc725242fd7fbfd341cefc
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-04-04 16:47:34 -0400

    Fix test for new patient model

--- frontend/src/components/NewPatientModel/NewPatientModel.test.tsx
+++ frontend/src/components/NewPatientModel/NewPatientModel.test.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { NewPatientModel } from ".";
 import { Mocks, Model } from "@dateam/shared";
 
 afterEach(cleanup);
 
 describe("DisplayItem", () => {
-    const sdcDisplayitemC: Model.SDCDisplayItem = Mocks.genDisplayItemComplete()
-    const sdcDisplayitemP: Model.SDCDisplayItem = Mocks.genDisplayItemPartial()
+    const history = createMemoryHistory();
+    const route = '/';
+    history.push(route);
 
     it('renders without errors for complete newpatientmodel', () => {
-        const { getByTestId } = render(<NewPatientModel
-            showModal={true}
-            goToRespones={false}
-        />);
+        const { getByTestId } = render(
+            <Router history={history}>
+                <NewPatientModel
+                    showModal={true}
+                    goToRespones={false}
+                />
+            </Router>,
+        );
         expect(getByTestId("newpatientmodel")).toBeVisible();
     });
 });
