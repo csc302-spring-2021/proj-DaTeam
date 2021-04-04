dafc6a3c412edd1f950a4441cf7227a43d2fde52
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2021-04-04 17:33:17 -0400

    Fix home tests

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -1,8 +1,45 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import {Home} from "../Home";
+import { Home } from "../Home";
+import { createMemoryHistory, MemoryHistory } from "history";
+import { render, screen } from "@testing-library/react";
+import { Router } from "react-router";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { GenericJsonSerializer, Mocks, Model } from "@dateam/shared";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-test('render check for home page', () => {
-    const {getByTestId} = render(<Home/>);
-    getByTestId("home")
+function renderHome(initialEntries = ["/home"]): { history: MemoryHistory } {
+    const queryClient = new QueryClient();
+
+    const history = createMemoryHistory({ initialEntries });
+    render(
+        <QueryClientProvider client={queryClient}>
+            <Router history={history}>
+                <Home />
+            </Router>
+        </QueryClientProvider>
+    );
+
+    return { history };
+}
+
+const server = setupServer(
+    rest.get("/api/v1/forms", (req, res, ctx) => {
+        const form1 = Mocks.genPatientComplete();
+        form1.uid = "patient1";
+        const json = [GenericJsonSerializer.encode(form1, Model.Patient)];
+        return res(ctx.json(json));
+    })
+);
+
+describe("Home", () => {
+    // Setup/teardown API mocking for all tests in this scope
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
+
+    it("renders home", async () => {
+        renderHome();
+        expect(screen.getByTestId("home")).toBeVisible();
+    });
 });
