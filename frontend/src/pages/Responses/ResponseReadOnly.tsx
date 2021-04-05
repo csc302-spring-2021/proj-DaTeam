7b7edf0e983938731fdec5a27d4168aa09451c48
Jin Chun <jinchun@Jins-MacBook-Pro.local>
2021-04-05 16:55:24 -0400

    use updated FormRenderer

---
+++ frontend/src/pages/Responses/ResponseReadOnly.tsx
@@ -0,0 +1,34 @@
+import { motion } from "framer-motion";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
+import { FormRenderer } from "../../components/FormRenderer";
+
+import { useForm, useFormResponse, usePatient } from "../../hooks/services";
+
+export function ResponseReadOnly() {
+  const { formId, responseId } = useParams<{
+    formId: string;
+    responseId: string;
+  }>();
+  const { data: form } = useForm(formId);
+  const { data: response } = useFormResponse(responseId);
+  const { data: patient } = usePatient(response?.patientID);
+
+  return (
+    <motion.div
+      data-testid="form-renderer-panel"
+      className="relative w-full min-h-full overflow-y-auto rounded-lg shadow-2xl sm:w-1/2 lg:w-1/2 bg-gray-50"
+    >
+      {form && (
+        <FormRenderer
+          key={formId + responseId}
+          form={form}
+          patient={patient}
+          sdcResponse={response}
+          readOnly={true}
+        />
+      )}
+    </motion.div>
+  );
+}
